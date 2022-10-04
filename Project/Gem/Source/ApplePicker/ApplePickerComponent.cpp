314cbeab3b91fc7820606a9fdff4731f2539d9f9
Michał Pełka <michalpelka@gmail.com>
2022-10-04 12:13:58 +0200

    Apple detection
    Signed-off-by: Michał Pełka <michalpelka@gmail.com>

--- Project/Gem/Source/ApplePicker/ApplePickerComponent.cpp
+++ Project/Gem/Source/ApplePicker/ApplePickerComponent.cpp
@@ -12,6 +12,13 @@
 #include <AzCore/Serialization/EditContext.h>
 #include <AzCore/Serialization/EditContextConstants.inl>
 
+#include <AtomLyIntegration/CommonFeatures/Mesh/MeshComponentBus.h>
+#include <AtomLyIntegration/CommonFeatures/Mesh/MeshComponentConstants.h>
+#include <AzCore/Component/TransformBus.h>
+#include <AzFramework/Physics/PhysicsSystem.h>
+#include <AzFramework/Physics/Shape.h>
+#include <AzToolsFramework/Entity/EditorEntityHelpers.h>
+
 namespace AppleKraken
 {
     namespace Internal
@@ -159,16 +166,71 @@ namespace AppleKraken
         }
     }
 
-    void ApplePickerComponent::QueryEnvironmentForAllApplesInBox(const AZ::Obb& /*globalBox*/)
+    void ApplePickerComponent::QueryEnvironmentForAllApplesInBox(const AZ::Obb& globalBox)
     {
         // TODO - query environment
 
-        // Debug
-        for (int i = 0; i < 5; ++i)
+        // Scene query for `apple` entity, we want visible entities with exact 'Apple' name
+        auto* physicsSystem = AZ::Interface<AzPhysics::SystemInterface>::Get();
+
+        // clear
+        m_currentAppleTasks = AZStd::queue<PickAppleTask>();
+        // apples need to be unique
+        AZStd::unordered_set<AZ::EntityId> found_apples;
+
+        for (auto& physicScene : physicsSystem->GetAllScenes())
         {
-            PickAppleTask emptyTask;
-            m_currentAppleTasks.push(emptyTask);
+            if (!physicScene)
+            {
+                continue;
+            }
+
+            const float scan_step = 0.1f;
+            // TODO whole scaning here is hack to deal with maximum number of 32 results that this
+            // CreateBoxOverlapRequest can handle. It supposed to be adjusted in but it is not
+            //  AzPhysics::SceneConfiguration. We create small box and go from -1 to 1 in Z.
+
+            AZ::Vector3 small_box_size;
+            small_box_size.SetX(2.0f * globalBox.GetHalfLengths().GetX());
+            small_box_size.SetY(2.0f * globalBox.GetHalfLengths().GetY());
+            small_box_size.SetZ(scan_step * globalBox.GetHalfLengths().GetZ());
+
+            for (float f = -1.f; f < 1.f; f += scan_step)
+            {
+                AzPhysics::OverlapRequest request = AzPhysics::OverlapRequestHelpers::CreateBoxOverlapRequest(
+                    small_box_size,
+                    AZ::Transform::CreateFromQuaternionAndTranslation(
+                        globalBox.GetRotation(), globalBox.GetPosition() + AZ::Vector3(0.f, 0.f, f)));
+
+                AzPhysics::SceneQueryHits results = physicScene->QueryScene(&request);
+                for (auto& r : results.m_hits)
+                {
+                    auto* entity = AzToolsFramework::GetEntity(r.m_entityId);
+                    AZ_Printf("ApplePickerComponent", "hit to %s : %s", r.m_entityId.ToString().c_str(), entity->GetName().c_str());
+                    if (entity->GetName() != "Apple")
+                    {
+                        continue;
+                    }
+                    if (found_apples.contains(r.m_entityId))
+                    {
+                        continue;
+                    }
+                    bool is_visible = false;
+                    AZ::Render::MeshComponentRequestBus::EventResult(
+                        is_visible, r.m_entityId, &AZ::Render::MeshComponentRequests::GetVisibility);
+                    if (!is_visible)
+                    {
+                        continue;
+                    }
+                    PickAppleTask t;
+                    t.m_appleEntityId = r.m_entityId;
+                    t.m_appleBoundingBox = r.m_shape->GetAabb(entity->GetTransform()->GetWorldTM());
+                    m_currentAppleTasks.push(t);
+                    found_apples.emplace(r.m_entityId);
+                }
+            }
         }
+        AZ_Printf("ApplePickerComponent", "There are %d apples in reach box \n", m_currentAppleTasks.size());
     }
 
 } // namespace AppleKraken
